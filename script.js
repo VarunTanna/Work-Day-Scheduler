1c4ff7df24146fb33e374cebd8fbc7fdc8bb37d1
VarunTanna <varuntanna@Varuns-MacBook-Pro.local>
2022-05-31 17:11:13 -0400

    linked js to html. finsihing up

--- script.js
+++ script.js
@@ -2,53 +2,56 @@
 var m = moment();
 
 //current date and time
-$("#currentDay").text(moment().format("LLLL"));
-
+$("#currentDay").text(moment().format("LL"));
+var currentTime = moment().hours();
 
 // color change 
-$(document).ready(function () {
-    colorChange();
-    renderText();
-});
-
 function colorChange() {
     var currentTime = moment().hours();
     console.log("Current Time" + currentTime);
 
 
-// past, present, future blocks if else statment. similar to display state from timed quiz
-$(".input").each(function () {
-    var time = parseInt($(this).attr("id"));
-    console.log(time);
+    // past, present, future blocks if else statment. similar to display state from timed quiz
+    $("textarea").each(function () {
+       
+        var time = parseInt($(this).parent().attr("id").substring(4));
+        console.log(currentTime, time);
 
         if (currentTime > time) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
+           
             $(this).addClass("past");
 
-         } else if (currentTime < time) {
-                $(this).addClass("future");
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-         } else {
-                $(this).removeClass("future");
-                $(this).addClass("present");
-                $(this).removeClass("past");
-         }
+        } else if (currentTime < time) {
+            $(this).addClass("future");
+            
+        } else {
+            
+            $(this).addClass("present");
+            
+        }
     });
 }
-
-// save button function v local storage
+    colorChange();
+    
+// save button function w/ local storage
 var dailyText;
 var dailyTime;
 
-$(".saveBtn").click( function(){
-    dailyText = $(this).siblings(".input").val("");
-    console.log(dailyTime);
-    dailyTime = $(this).siblings(".input").text();
-    console.log(dailyTime);
-    localStorage.setItem(dailyTime,JSON.stringify(dailyText));
+$("button").on("click", function (event) {
+
+    localStorage.setItem($(event.target).parent().attr("id"), $(event.target).parent().children().eq(1).val());
+    // dailyText = $(this).siblings(".input").val("");
+    // console.log(dailyTime);
+    // dailyTime = $(this).siblings(".input").text();
+    // console.log(dailyTime);
+    // localStorage.setItem(dailyTime,JSON.stringify(dailyText));
+
+  
 });
+
+
+
+
 
 
 // function for what the currrent time is
@@ -58,12 +61,11 @@ $(".saveBtn").click( function(){
 
 
 // local storage
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17.description").val(localStorage.getItem("hour17"));
+for (i = 9; i <= 17; i++) {
+$("#hour"+i).children().eq(1).val(localStorage.getItem("hour" +i));
+
+}
+
+
+
+
