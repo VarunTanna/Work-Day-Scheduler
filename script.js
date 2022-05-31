6d0ac39436b4bd6ef58be6f59320429398a701ff
VarunTanna <varuntanna@Varuns-MacBook-Pro.local>
2022-05-31 15:32:28 -0400

    local storage in js

--- script.js
+++ script.js
@@ -9,9 +9,20 @@ $("#currentDay").hmtl(date);
 
 
 // function for what the currrent time is
+function time() {
 
+}
 
 
 
 
 // local storage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17.description").val(localStorage.getItem("hour17"));
