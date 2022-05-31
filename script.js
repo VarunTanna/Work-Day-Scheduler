2e67e9c0e86396f87f911fa25edd735e805907ad
VarunTanna <varuntanna@Varuns-MacBook-Pro.local>
2022-05-31 16:02:49 -0400

    event blocks done and starting savbtn

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
 //moment.js 
-var date = moment().format("dddd,MMM Do YYYY");
-$("#currentDay").hmtl(date);
+var m = moment();
+
+//current date and time
+$("#currentDay").text(moment().format("LLLL"));
+
+
+// color change 
+$(document).ready(function () {
+    colorChange();
+    renderText();
+});
+
+function colorChange() {
+    var currentTime = moment().hours();
+    console.log("Current Time" + currentTime);
+
+
+// past, present, future blocks if else statment. similar to display state from timed quiz
+$(".input").each(function () {
+    var time = parseInt($(this).attr("id"));
+    console.log(time);
+
+        if (currentTime > time) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+
+         } else if (currentTime < time) {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+         } else {
+                $(this).removeClass("future");
+                $(this).addClass("present");
+                $(this).removeClass("past");
+         }
+    });
+}
 
 // save button function v local storage
+var dailyText;
+var dailyTime;
 
-
-
+$(".saveBtn").click( function(){
+    dailyText = $(this).siblings(".input").val("");
+    console.log(dailyTime);
+    dailyTime = $(this).siblings(".input").text();
+    console.log(dailyTime);
+    localStorage.setItem(dailyTime,JSON.stringify(dailyText));
+});
 
 
 // function for what the currrent time is
-function time() {
 
-}
 
 
 
